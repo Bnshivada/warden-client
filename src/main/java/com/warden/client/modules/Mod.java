c52fec666df94a1b6e00b8638093ae99081f5df8
Shivada <mehmeterensariot15@gmail.com>
2026-02-04 22:24:51 +0300

    Update Mod.java

--- src/main/java/com/warden/client/modules/Mod.java
+++ src/main/java/com/warden/client/modules/Mod.java
@@ -1,14 +1,23 @@
 package com.warden.client.modules;
 
+import com.warden.client.settings.NumberSetting;
 import net.minecraft.client.MinecraftClient;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mod {
     public String name;
     public boolean enabled = false;
+    public boolean expanded = false; // Menüde ayarları açık mı?
+    public List<NumberSetting> settings = new ArrayList<>(); // Ayar listesi
     protected static MinecraftClient mc = MinecraftClient.getInstance();
 
     public Mod(String name) {
         this.name = name;
+    }
+
+    public void addSetting(NumberSetting setting) {
+        settings.add(setting);
     }
 
     public void toggle() {
