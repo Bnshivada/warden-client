8d635a112b43daec6bac585f5f37b03dc4a20627
Shivada <mehmeterensariot15@gmail.com>
2026-02-04 23:52:41 +0300

    Update Reach.java

--- src/main/java/com/warden/client/modules/Reach.java
+++ src/main/java/com/warden/client/modules/Reach.java
@@ -6,9 +6,7 @@ public class Reach extends Mod {
     public static NumberSetting reachDistance = new NumberSetting("Mesafe", 4.0, 3.0, 6.0, 0.1);
 
     public Reach() {
-        super("Reach");
+        super("Reach", Category.COMBAT);
         addSetting(reachDistance);
     }
-    
-    // Not: Gerçek Reach için Mixin gerekir, ancak KillAura bu değeri okuyacak.
 }
