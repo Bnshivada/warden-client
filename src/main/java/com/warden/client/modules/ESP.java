7999d53e08ebb314fa092be4076d6030e36bc2b4
Shivada <mehmeterensariot15@gmail.com>
2026-02-04 22:25:53 +0300

    Update ESP.java

--- src/main/java/com/warden/client/modules/ESP.java
+++ src/main/java/com/warden/client/modules/ESP.java
@@ -3,27 +3,6 @@ package com.warden.client.modules;
 import net.minecraft.entity.player.PlayerEntity;
 
 public class ESP extends Mod {
-    public ESP() {
-        super("ESP");
-    }
-
-    @Override
-    public void onTick() {
-        if (!enabled || mc.world == null) return;
-
-        mc.world.getEntities().forEach(entity -> {
-            if (entity instanceof PlayerEntity && entity != mc.player) {
-                // Glow (parlama) efekti vererek duvar arkasından görmeyi sağlar
-                entity.setGlowing(true);
-            }
-        });
-    }
-
-    @Override
-    public void onDisable() {
-        // Hile kapandığında parlamayı kapat
-        if (mc.world != null) {
-            mc.world.getEntities().forEach(entity -> entity.setGlowing(false));
-        }
-    }
+    public ESP() { super("ESP"); }
+    // Render işlemi WardenClient.java içinde yapılacak, burası sadece aç/kapa mantığı.
 }
