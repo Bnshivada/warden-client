13327a832c878bbcadc518802b2a52a558eddc26
Shivada <mehmeterensariot15@gmail.com>
2026-02-04 23:53:11 +0300

    Update Xray.java

--- src/main/java/com/warden/client/modules/Xray.java
+++ src/main/java/com/warden/client/modules/Xray.java
@@ -1,32 +1,21 @@
 package com.warden.client.modules;
 
-import net.minecraft.client.MinecraftClient;
-
 public class Xray extends Mod {
-    public Xray() {
-        super("Xray");
-    }
+    public Xray() { super("Xray", Category.RENDER); }
 
-    // Bu değişkeni oyunun blokları şeffaf mı çizmesi gerektiğini anlaması için kullanacağız
     public static boolean isXrayEnabled = false;
 
     @Override
     public void onEnable() {
         isXrayEnabled = true;
-        // 1.21.4'te parlaklığı (gamma) arttırarak karanlık yerleri görmeyi sağlar
         mc.options.getGamma().setValue(16.0); 
-        // Dünyayı yeniden yükle ki bloklar yeni ayara göre çizilsin
-        if (mc.worldRenderer != null) {
-            mc.worldRenderer.reload();
-        }
+        if (mc.worldRenderer != null) mc.worldRenderer.reload();
     }
 
     @Override
     public void onDisable() {
         isXrayEnabled = false;
-        mc.options.getGamma().setValue(1.0); // Parlaklığı normale döndür
-        if (mc.worldRenderer != null) {
-            mc.worldRenderer.reload();
-        }
+        mc.options.getGamma().setValue(1.0);
+        if (mc.worldRenderer != null) mc.worldRenderer.reload();
     }
 }
